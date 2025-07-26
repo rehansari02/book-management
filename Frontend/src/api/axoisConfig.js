e1146393a127e87b20206b7db57555dd9d183bc0
Ansari Rehan <rehansari798@gmail.com>
2025-07-27 03:43:04 +0530

    Update axoisConfig.js
    link rename

--- Frontend/src/api/axoisConfig.js
+++ Frontend/src/api/axoisConfig.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://book-management-19jv.onrender.com",
+  baseURL: "https://book-management-19jv.onrender.com/api",
 });
 
 export default axiosInstance;
